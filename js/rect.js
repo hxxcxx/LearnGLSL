c2d1b25e3402b360ba5704e664d83b93fb156c4c
hxxcxx <55966611+hxxcxx@users.noreply.github.com>
2025-04-14 00:52:19 +0800

    圆范围

--- js/rect.js
+++ js/rect.js
@@ -177,6 +177,60 @@ void main (void)
   gl_FragColor = vec4(color, 1.0); 
 }
 `
+const vshader_fract = `
+varying vec2 vUv;
+varying vec3 vPosition;
+
+void main() {	
+  vUv = uv;
+  vPosition = position;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+`
+const fshader_fract = `
+#define PI2 6.28318530718
+
+uniform vec2 u_mouse;
+uniform vec2 u_resolution;
+uniform float u_time;
+uniform vec3 u_color;
+
+varying vec2 vUv;
+varying vec3 vPosition;
+
+float rect(vec2 pt, vec2 anchor, vec2 size, vec2 center){
+  //return 0 if not in rect and 1 if it is
+  //step(edge, x) 0.0 is returned if x < edge, and 1.0 is returned otherwise.
+  vec2 p = pt - center;
+  vec2 halfsize = size/2.0;
+  float horz = step(-halfsize.x - anchor.x, p.x) - step(halfsize.x - anchor.x, p.x);
+  float vert = step(-halfsize.y - anchor.y, p.y) - step(halfsize.y - anchor.y, p.y);
+  return horz*vert;
+}
+
+mat2 getRotationMatrix(float theta){
+  float s = sin(theta);
+  float c = cos(theta);
+  return mat2(c, -s, s, c);
+}
+
+mat2 getScaleMatrix(float scale){
+  return mat2(scale,0,0,scale);
+}
+
+void main (void)
+{
+  float tilecount = 6.0;
+  vec2 center = vec2(0.5);
+  vec2 pt = fract(vUv*tilecount) - center;
+  mat2 matr = getRotationMatrix(u_time);
+  mat2 mats = getScaleMatrix((sin(u_time)+1.0)/3.0 + 0.5);
+  pt = mats * matr * pt;
+  pt += center;
+  vec3 color = u_color * rect(pt, vec2(0.0), vec2(0.3), center);
+  gl_FragColor = vec4(color, 1.0); 
+}
+`
 
 
 
@@ -200,8 +254,8 @@ const uniforms = {
 
 const material = new THREE.ShaderMaterial( {
   uniforms: uniforms,
-  vertexShader: vshader,
-  fragmentShader: fshader_anchor
+  vertexShader: vshader_fract,
+  fragmentShader: fshader_fract
 } );
 
 const plane = new THREE.Mesh( geometry, material );
