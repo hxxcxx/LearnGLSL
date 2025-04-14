09e9b34c123a5787d7a050ed802d2055c28c1a9f
hxxcxx <55966611+hxxcxx@users.noreply.github.com>
2025-04-14 20:03:35 +0800

    ShaderScene封装

---
+++ js/ShaderScene.js
@@ -0,0 +1,71 @@
+class ShaderScene {
+  constructor(vshader, fshader, uniforms) {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+    this.renderer = new THREE.WebGLRenderer();
+    this.clock = new THREE.Clock();
+
+    this.uniforms = uniforms;
+    
+    this.material = new THREE.ShaderMaterial({
+      uniforms: this.uniforms,
+      vertexShader: vshader,
+      fragmentShader: fshader,
+    });
+
+    this.plane = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), this.material);
+    this.scene.add(this.plane);
+
+    this.camera.position.z = 1;
+
+    this.initRenderer();
+    this.addEventListeners();
+    this.onWindowResize();
+    this.animate();
+  }
+
+  initRenderer() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+  }
+
+  addEventListeners() {
+    if ('ontouchstart' in window) {
+      document.addEventListener('touchmove', this.move.bind(this));
+    } else {
+      window.addEventListener('resize', this.onWindowResize.bind(this), false);
+      document.addEventListener('mousemove', this.move.bind(this));
+    }
+  }
+
+  move(evt) {
+    this.uniforms.u_mouse.value.x = evt.touches ? evt.touches[0].clientX : evt.clientX;
+    this.uniforms.u_mouse.value.y = evt.touches ? evt.touches[0].clientY : evt.clientY;
+  }
+
+  onWindowResize() {
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    let width, height;
+    if (aspectRatio >= 1) {
+      width = 1;
+      height = (window.innerHeight / window.innerWidth) * width;
+    } else {
+      width = aspectRatio;
+      height = 1;
+    }
+    this.camera.left = -width;
+    this.camera.right = width;
+    this.camera.top = height;
+    this.camera.bottom = -height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.uniforms.u_resolution.value.x = window.innerWidth;
+    this.uniforms.u_resolution.value.y = window.innerHeight;
+  }
+
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    this.uniforms.u_time.value += this.clock.getDelta();
+    this.renderer.render(this.scene, this.camera);
+  }
+}
